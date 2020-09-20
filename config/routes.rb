8ce2874a74867ffe836ad26c99ca78429a13afad
Thiago <thiagodsalles@gmail.com>
2020-09-20 16:03:41 -0300

    add routes to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :question_accesses
+      resources :questions
+      resources :disciplines
+      resources :file_uploads
+    end
+  end
 end
