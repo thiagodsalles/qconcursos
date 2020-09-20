3cceb73d3ba6dcd8114ef712643946448385dd25
Thiago <thiagodsalles@gmail.com>
2020-09-20 16:55:18 -0300

    add validations on question_access model

--- app/models/question_access.rb
+++ app/models/question_access.rb
@@ -1,3 +1,6 @@
 class QuestionAccess < ApplicationRecord
   belongs_to :question
+
+  validates :date, :times_accessed, presence: true
+  validates :times_accessed, numericality: { only_integer: true }
 end
