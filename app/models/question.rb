f7bf66053e7cadb3d2813b2bbdba73e42da44c5b
Thiago <thiagodsalles@gmail.com>
2020-09-20 16:55:08 -0300

    add validations on question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   has_many :question_accesses
+
+  validates :statement, :text, :answer, :daily_access, :discipline, :created_at, presence: true
+  validates :daily_access, numericality: { only_integer: true }
 end
